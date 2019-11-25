84f486b1b62a08d97ded9247562d7b6162bd619f
giorgiomartini <jorgeantonio82@googlemail.com>
2019-11-25 18:34:11 +0100

    first commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,17 @@
-console.log('welcome on board!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hella 
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -33,20 +33,7 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.mjs$/,
-        include: /node_modules/,
-        type: 'javascript/auto'
-      },
-      {
-        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[path][name].[ext]'
-          }
-        }
-      },
-    ]
+      { test: /\.(js)$/, use: ['babel-loader',  'eslint-loader'], exclude: /node_modules/},
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}    ]
   }
 };
