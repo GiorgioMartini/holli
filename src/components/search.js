dc6e82c69aff15d8f7fff982dcc3a3a4d58a9667
giorgiomartini <jorgeantonio82@googlemail.com>
2019-11-27 00:23:22 +0100

    added filtering stars

--- src/components/search.js
+++ src/components/search.js
@@ -5,19 +5,42 @@ import styles from './results.scss'
 import { FlightResults } from './flightResults'
 
 export function Search ({ endpoint, title = 'Flights for your next adventure' }) {
-  const [search, setsearch] = useState([])
+  const [results, setresults] = useState([])
+  const [filteredResults, setFilteredResults] = useState([])
+  const [stars, setStars] = useState([])
+  const ratings = [1,2,3,4,5]
 
   useEffect(() => {
     fetchHotels(endpoint)
-      .then( data => setsearch(data))
-      .catch(e => console.log(e))
-  }, [endpoint])
+      .then( data => {
+        setFilteredResults(data)
+        setresults(data)
+      })
+      .catch(e => console.log('MAKE SOMETHING IN UI', e))
+    }, [endpoint])
+
+  
+  function handleStarHover(starRate) {
+    setStars(starRate)
+    setFilteredResults(results)
+    setFilteredResults(results.filter(x =>  x.stars <= starRate))
+  }
+
 
   return (
     <div className=''>
       <h2 className=''>{title}</h2>
+
+      {ratings.map( (starRate, i) => (
+        <span
+          key={i}
+          onMouseEnter={() => handleStarHover(starRate)}>
+          ★
+        </span>
+      ))}
+
         <FlightResults
-          flights={search}
+          flights={filteredResults}
         />
     </div>
   )
